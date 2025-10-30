c87ad54a284405fc988e0458b16addfaa98c3b18
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-30 16:32:46 +0700

    Tambah HorizontalDivider pada FormDataDiri dengan padding atas dan bawah untuk pemisah antar elemen

--- app/src/main/java/com/example/tugaspertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/tugaspertemuan5/TeksEdit.kt
@@ -109,6 +109,13 @@ fun FormDataDiri(modifier: Modifier
         ){
             Text(text = stringResource(id = R.string.submit))
         }
+        HorizontalDivider(
+            modifier = Modifier
+                .padding(bottom = dimensionResource(id = R.dimen.padding_medium),
+                    top = dimensionResource(
+                        id = R.dimen.padding_medium
+                    )),
+
 
 
 
