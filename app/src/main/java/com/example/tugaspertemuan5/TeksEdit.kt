32ad1c1b1ee2f7a094c91e6a47b24648a19022a6
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-30 16:14:31 +0700

    Tambah komponen FormDataDiri untuk input data pribadi dengan variabel state nama, alamat, jenis, dan daftar gender

--- app/src/main/java/com/example/tugaspertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/tugaspertemuan5/TeksEdit.kt
@@ -41,3 +41,9 @@ fun FormDataDiri(modifier: Modifier
     var textalamat by remember { mutableStateOf(value = "") }
     var textJK by remember { mutableStateOf(value = "") }
 
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
+
+    val gender:List<String> = listOf("Laki-laki","perempuan")
+
